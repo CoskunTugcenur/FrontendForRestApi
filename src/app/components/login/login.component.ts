33ef6c9ce210c23fb89bab58ada55e1416cc51b4
TugcenurC <coskuntugcenur@gmail.com>
2021-03-25 02:51:17 +0300

    LoginComponent changed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormBuilder,Validators} from '@angular/forms';
+import {FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
@@ -9,15 +9,24 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
 
   loginForm:FormGroup;
-  constructor(private authService:AuthService) { }
+  constructor(private formBuilder:FormBuilder,private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
 
+  createLoginForm(){
+    this.loginForm=this.formBuilder.group({
+      email:["",Validators.required],
+      password:["",Validators.required]
+    });
+  }
+  
   login(){
     if (this.loginForm.valid){
       let loginModule=Object.assign({},this.loginForm.value);
-      
+      console.log(loginModule);
+    
     }
   }
 
